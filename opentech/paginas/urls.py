9b31ebe6fbc97cdd769f52d7a4c78ecd0f6eaa2d
PedroMiceli <pedro.miceli@hotmail.com>
2022-08-12 17:42:46 -0300

    adicionadas as paginas html do felipe

--- opentech/paginas/urls.py
+++ opentech/paginas/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cliente', views.cliente, name='cliente'),
+    path('localizacao', views.localizacao, name='localizacao'),
+    path('login', views.login, name='login'),
+    path('servicos', views.servicos, name='servicos'),
+    path('sobre', views.sobre, name='sobre'),
+    path('vagas', views.vagas, name='vagas'),
+
 ]
--- opentech/paginas/views.py
+++ opentech/paginas/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def cliente(request):
+    return render(request, 'cliente.html')
+
+def localizacao(request):
+    return render(request, 'localizacao.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def servicos(request):
+    return render(request, 'servicos.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def vagas(request):
+    return render(request, 'vagas.html')
+
+
